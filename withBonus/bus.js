ea3a4cfe17c8711b81d91007402b522c8aca30b5
gsgallant <Greg@Gregs-MacBook-Pro.local>
2016-04-13 13:26:23 -0400

    refactored code.  Used two custom callbacks.  Better user experience

--- withBonus/bus.js
+++ withBonus/bus.js
@@ -1,24 +1,9 @@
 var Student = require('./student.js');
 var fs = require('fs');
 var colors=require('colors');
+var pad = require('pad');
 
-function studentsStillOnBus(){
-			fs.readFile("schoolbus.txt", "utf-8", function(err, readResult){
-					if(err)
-						throw err;
-							else{	
-									var students = readResult.split('\r\n');
-						for (var i=0; i< students.length-1; i++){
-							var itemJSON = JSON.parse(students[i].replace(/[\[\]']+/g,''));
-								if(i%7==0){console.log("");}
-								process.stdout.write(itemJSON.name.bold.red);
-								if(i<students.length-2){process.stdout.write(", ".red);}
-								// console.log(itemJSON.name.bold.red);
-						}
-						console.log("\n");
-					}
-			})
-}
+
 var Bus = function(studentsOnTheBus,driverName,color,gas){
 	this.studentsOnTheBus = [];
 	this.driverName = driverName;
@@ -35,7 +20,7 @@ var Bus = function(studentsOnTheBus,driverName,color,gas){
 				throw err;
 			else{		
 					var students = readResult.split('\r\n');
-					if (students.length>=startChatterTrigger){
+					if (students.length-1>=startChatterTrigger){
 						console.log("\nThe students are making noise!".red.bold);
 						for (var i=0; i< students.length-1; i++){
 							var itemJSON = JSON.parse(students[i].replace(/[\[\]']+/g,''));
@@ -45,9 +30,6 @@ var Bus = function(studentsOnTheBus,driverName,color,gas){
 								console.log(itemJSON.name.red.bold+ " MUST STAY **QUIET!!**".bold.red);
 							}
 						}
-					}else{
-						console.log("\nThese students are on the bus".green);
-						studentsStillOnBus();
 					}
 					console.log("\n");
 					callback();
@@ -66,19 +48,16 @@ var Bus = function(studentsOnTheBus,driverName,color,gas){
 							var students = readResult.split('\r\n');
 							var who = name;
 							var nameRemoved = false;
-							if (!who){
-								who="Nobody"
-								console.log("\n"+who.bold.red + " was thrown off the bus".red);
+							if (!name){
+								console.log("\nNobody".bold.red + " was thrown off the bus".red);
 								};
-								if(name){
+							if(name){
 									fs.writeFile("schoolbus.txt","");//clear out the txt file	
-									console.log("The following students are still on the bus:".bold.green);
 									for (var i=0; i< students.length-1; ++i){
 
 										var itemJSON = JSON.parse(students[i].replace(/[\[\]']+/g,''));
 
 										if(name != itemJSON.name) {
-										console.log(itemJSON.name.bold.red);
 										fs.appendFile("schoolbus.txt", students[i] + "\r\n", function(err){
 												if(err)
 													throw err;
@@ -92,12 +71,32 @@ var Bus = function(studentsOnTheBus,driverName,color,gas){
 									}else{
 										console.log("\n"+name.bold.red + " was NOT ON the bus".red);
 									}
-								}else{ 
-										console.log("\nThese students are still on the bus".green);
-										studentsStillOnBus();
-									}		
-							}
+								}
+						}
 			})
 	}
+
+	this.stillOnBus = function(callback){
+				fs.readFile("schoolbus.txt", "utf-8", function(err, readResult){
+						if(err)
+							throw err;
+								else{	
+									var students = readResult.split('\r\n');
+									console.log("\nThese students are on the bus");
+									for (var i=0; i< students.length-1; i++){
+										var itemJSON = JSON.parse(students[i].replace(/[\[\]']+/g,''));
+										
+										var displayText = pad(itemJSON.name,10);
+
+										if(i%5==0){console.log("");}
+										process.stdout.write(displayText.bold.red);
+										// if(i<students.length-2){process.stdout.write(", ".red);}
+									}
+								console.log("\n");
+								callback();
+						}
+				})
+	}
+
 }
 module.exports = Bus;
--- withBonus/main.js
+++ withBonus/main.js
@@ -5,35 +5,39 @@ var fs = require('fs');
 var startChatterTrigger = 20;///used a variable because I used this for testing and set it to a small number.  HW requires 20 
 var newBus = new Bus();
 
-prompt.start();
-	
-prompt.get(['name','gender','grade','GPA','detentions','sleepingInClass','catchPhrase'],function(err,result){
+newBus.stillOnBus(function(){
+
+	console.log("Who would you like to add?");
+	prompt.start();
 		
-		if (result.name){
-			var newStudent = new Student(result.name,result.gender,result.grade,result.GPA,result.detentions,result.sleepingInClass,result.catchPhrase);
-
-			newStudent.canStudentHaveFun();
-
-			newBus.studentEntersBus(result.name,result.gender,result.grade,result.GPA,result.detentions,result.sleepingInClass,result.catchPhrase);
-
-			fs.appendFile("schoolbus.txt", JSON.stringify(newBus.studentsOnTheBus) + "\r\n", function(err){
-					if(err)
-						throw err;
-				})
+	prompt.get(['name','gender','grade','GPA','detentions','sleepingInClass','catchPhrase'],function(err,result){
 			
-		}
-			newBus.busChatter(startChatterTrigger,function(){
-					console.log("Who would you like to remove from the bus? <hit enter for no one>")
-					prompt.start();
-					
-					prompt.get(['name'],function(err,result){
-					if(!err){
-							newBus.removeStudentFromBus(result.name);
-							}else{
-								throw err;
-							}
-								
-					})
-			})
-});
+			if (result.name){
+				var newStudent = new Student(result.name,result.gender,result.grade,result.GPA,result.detentions,result.sleepingInClass,result.catchPhrase);
 
+				newStudent.canStudentHaveFun();
+
+				newBus.studentEntersBus(result.name,result.gender,result.grade,result.GPA,result.detentions,result.sleepingInClass,result.catchPhrase);
+
+				fs.appendFile("schoolbus.txt", JSON.stringify(newBus.studentsOnTheBus) + "\r\n", function(err){
+						if(err)
+							throw err;
+					})
+				
+			}
+				
+				newBus.busChatter(startChatterTrigger,function(){
+						console.log("Who would you like to remove from the bus? <hit enter for no one>")
+						prompt.start();
+						
+						prompt.get(['name'],function(err,result){
+						if(!err){
+								newBus.removeStudentFromBus(result.name);
+								}else{
+									throw err;
+								}
+						})
+				})
+	})
+
+})
